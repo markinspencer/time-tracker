f7b9818007616c4c650ce702f30e4cc0140866d0
markinspencer <markinspencer@gmail.com>
2019-07-31 09:13:55 -0400

    display time as hh:mm:ss

--- client/src/components/StopWatch.jsx
+++ client/src/components/StopWatch.jsx
@@ -6,19 +6,23 @@ class StopWatch extends Component {
   timer;
 
   start = () => {
-    const { interval } = this.props.viewState.stopwatch;
-    this.timer = setInterval(this.props.tick, interval);
+    if (!this.timer) {
+      const { interval } = this.props.viewState.stopwatch;
+      this.timer = setInterval(this.props.tick, interval);
+    }
   };
 
   stop = () => {
     clearInterval(this.timer);
+    this.timer = null;
   };
 
   render() {
     const { seconds } = this.props.viewState.stopwatch;
+    const time = new Date(seconds * 1000).toISOString().substr(11, 8);
     return (
       <div>
-        <div> {seconds} </div>
+        <div> {time} </div>
         <button onClick={() => this.start()}> Start </button>
         <button onClick={() => this.stop()}> Stop </button>
       </div>
